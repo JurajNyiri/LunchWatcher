c7d73ecfc50ad844f63be8fdf689b9b43ab73749
Juraj Nyiri <jnyiri@sk.ibm.com>
2016-03-08 11:55:11 +0100

    Added kantinka

--- zomato_supported.php
+++ zomato_supported.php
@@ -1,6 +1,7 @@
 <?php
 $zomatosupported = array(
 	"beabout" => "https://www.zomato.com/sk/bratislava/beabout-star%C3%A9-mesto-bratislava-i/menu",
-	"anjou" => "https://www.zomato.com/sk/bratislava/brasserie-anjou-star%C3%A9-mesto-bratislava-i/menu"
+	"anjou" => "https://www.zomato.com/sk/bratislava/brasserie-anjou-star%C3%A9-mesto-bratislava-i/menu",
+	"kantinka" => "https://www.zomato.com/sk/kantinka/menu"
 	);
 ?>
