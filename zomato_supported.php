dbb41dec172529be6156f157034210b551b60a7f
Juraj Nyiri <jnyiri@sk.ibm.com>
2016-03-08 12:59:28 +0100

    Added stuff

--- zomato_supported.php
+++ zomato_supported.php
@@ -1,8 +1,8 @@
 <?php
 $zomatosupported = array(
-	"beabout" => "https://www.zomato.com/sk/bratislava/beabout-star%C3%A9-mesto-bratislava-i/menu",
-	"anjou" => "https://www.zomato.com/sk/bratislava/brasserie-anjou-star%C3%A9-mesto-bratislava-i/menu",
-	"kantinka" => "https://www.zomato.com/sk/kantinka/menu",
-	"vikaro" => "https://www.zomato.com/sk/bratislava/vikaro-002-star%C3%A9-mesto-bratislava-i/menu"
+	"beabout" => "https://www.zomato.com/sk/bratislava/beabout-star%C3%A9-mesto-bratislava-i/menu", //done (prices bug when they are not specified)
+	"anjou" => "https://www.zomato.com/sk/bratislava/brasserie-anjou-star%C3%A9-mesto-bratislava-i/menu", //done, without function
+	"kantinka" => "https://www.zomato.com/sk/kantinka/menu", //done, without function
+	"vikaro" => "https://www.zomato.com/sk/bratislava/vikaro-002-star%C3%A9-mesto-bratislava-i/menu" //TODO, needs function to correct stuff
 	);
 ?>
